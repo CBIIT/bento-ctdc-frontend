622e5ac8e893783a7e0f0308089a8fe073aff96e
Karan Sheth <karan.sheth60@gmail.com>
2021-08-06 08:29:13 -0400

    Replaced header image.

--- src/bento/globalHeaderData.js
+++ src/bento/globalHeaderData.js
@@ -4,5 +4,5 @@ export default {
   globalHeaderLogo: 'https://raw.githubusercontent.com/CBIIT/datacommons-assets/ctdc_Assets/ctdc/images/svg/CTDC_Logo.svg',
   globalHeaderLogoLink: '/',
   globalHeaderLogoAltText: 'CTDC Logo',
-  globalHeaderImage: 'https://raw.githubusercontent.com/CBIIT/datacommons-assets/ctdc_Assets/ctdc/images/png/header_easter2000.png',
+  globalHeaderImage: 'https://raw.githubusercontent.com/CBIIT/datacommons-assets/ctdc_Assets/ctdc/images/png/CTDC.bnr.2000x100.1.png',
 };
