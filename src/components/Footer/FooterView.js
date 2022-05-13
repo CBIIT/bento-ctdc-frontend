216f0557bd10c9ca72cd119f4fc6a9b1d75ca645
skiran86 <skphp9@gmail.com>
2022-05-13 15:04:51 -0400

    CTDC-784 Backend version from API

--- src/components/Footer/FooterView.js
+++ src/components/Footer/FooterView.js
@@ -1,10 +1,42 @@
 /* eslint-disable */
-
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Footer } from 'bento-components';
-import FooterData from '../../bento/globalFooterData.js';
+import gql from 'graphql-tag';
+import FooterData from '../../bento/globalFooterData';
+import env from '../../utils/env';
+import client from '../../utils/graphqlClient';
 
-// import nihLogo from '../../assets/header/icdc_nih_logo.svg';
+const GET_BE_VERSION = gql`{
+  schemaVersion
+}
+`;
 
-const ICDCFooter = () => <><Footer data={FooterData} background={FooterData.bg} /></>;
+const FILE_SERVICE_API = env.REACT_APP_FILE_SERVICE_API;
+
+const ICDCFooter = () => {
+  const [footerUpdatedData, setFooterUpdatedData] = useState(FooterData);
+  async function getBEVersion() {
+    const schemaVersion = await client
+      .query({
+        query: GET_BE_VERSION,
+      })
+      .then((result) => result.data.schemaVersion);
+    return schemaVersion;
+  }
+  useEffect(() => {
+    const getSystems = async () => {
+      const response = await fetch(
+        `${FILE_SERVICE_API}version`,
+      ).then((resp) => (resp))
+        .catch(() => ({ version: '' }));
+      const data = response.json();
+      const beVersion = await getBEVersion();
+      setFooterUpdatedData({ ...FooterData, ...{ FileServiceVersion: data.version || '' }, ...{ BEversion: beVersion } });
+    };
+    getSystems();
+  }, [FooterData]);
+
+  return <><Footer data={footerUpdatedData} /></>;
+};
+
 export default ICDCFooter;
