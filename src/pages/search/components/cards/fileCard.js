b73c9ee60a40d7dd011178ee6cbc7487bd681d22
skiran86 <skphp9@gmail.com>
2022-05-13 13:41:25 -0400

    CTDC-811 Remove unwanted links from global search cards

--- src/pages/search/components/cards/fileCard.js
+++ src/pages/search/components/cards/fileCard.js
@@ -1,6 +1,5 @@
 import { Grid, withStyles } from '@material-ui/core';
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { prepareLinks } from 'bento-components';
 import PropertyItem from '../propertyItem';
 
@@ -40,12 +39,6 @@ const FileCard = ({ data, classes, index }) => {
         <Grid item xs={11} className={classes.propertyContainer}>
           <div>
             <span className={classes.detailContainerHeader}>FILE</span>
-            <span className={classes.cardTitle}>
-              <Link to={`/case/${data.subject_id}`} className={classes.cardTitle}>
-                {data.file_id}
-              </Link>
-            </span>
-
           </div>
           {propertiesWithLinks.map((prop) => (
             <PropertyItem
