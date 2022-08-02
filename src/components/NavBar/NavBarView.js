66b4ab5d31b9405a4cb5d69bdb78780af383685e
skiran86 <skphp9@gmail.com>
2022-08-02 17:44:49 -0400

    CTDC-853 Reorganize Navigation Bar

--- src/components/NavBar/NavBarView.js
+++ src/components/NavBar/NavBarView.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { NavBar } from 'bento-components';
 // import { useSelector } from 'react-redux';
 import {
-  navBarData, navBarCartData, navBarstyling,
+  navBarData, navBarCartData, navBarstyling, externalLinks,
 } from '../../bento/navigationBarData';
 
 const BentoNavBar = ({ cartFieldIds }) => (
@@ -12,6 +12,8 @@ const BentoNavBar = ({ cartFieldIds }) => (
       navBarCartData={navBarCartData}
       navBarstyling={navBarstyling}
       numberOfCases={cartFieldIds.length || 0}
+      externalLinksFlag
+      externalLinks={externalLinks}
     />
   </>
 );
